f7b413c90884702ceedb8050bfc36cd3f5bb90ec
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-06-13 16:38:47 +0200

    Adds timezone and hashtags

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const getData = require('./lib/get-data')
 const tweetWarning = require('./lib/tweet-warning')
-const zeroPadding = input => input.toString().length === 1 ? `0${input}` : `${input}`
-function createMessage(level, areas) {
-  const now = new Date()
-  const list = areas.map(area => area.area)
-  return `Det er målt ${level} luftforurensning, kl ${zeroPadding(now.getHours())}:${zeroPadding(now.getMinutes())}, i ${list.join(', ')}. https://luftstatus.no `
-}
+const createMessage = require('./lib/create-message')
 
 module.exports.handler = async (event, context, callback) => {
   const response = await getData()
@@ -13,11 +8,11 @@ module.exports.handler = async (event, context, callback) => {
   let jobs = []
   if (data.high.length > 0) {
     const msg = createMessage('høy', data.high)
-    jobs.push(tweetWarning(msg)) 
+    jobs.push(tweetWarning(msg))
   }
   if (data.veryHigh.length > 0) {
     const msg = createMessage('svært høy', data.veryHigh)
-    jobs.push(tweetWarning(msg)) 
+    jobs.push(tweetWarning(msg))
   }
   const tweets = await Promise.all(jobs)
   return tweets
