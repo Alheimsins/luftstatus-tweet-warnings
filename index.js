55d57af732fa70fff7d976517b3309ec66e68b18
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-06-16 21:47:59 +0200

    Adds handling of gzipped json

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const getData = require('./lib/get-data')
 const tweetWarning = require('./lib/tweet-warning')
 const createMessage = require('./lib/create-message')
+const gunzipData = require('./lib/gunzip-data')
 
 module.exports.handler = async (event, context, callback) => {
   const response = await getData()
-  const data = JSON.parse(response.Body.toString('utf-8'))
+  const json = gunzipData(response.Body)
+  const data = JSON.parse(json.toString('utf-8'))
   let jobs = []
   if (data.high.length > 0) {
     const msg = createMessage('høy', data.high)
